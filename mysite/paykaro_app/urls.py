19816264c483dbef12762ad613f0277880ddcbd4
Ravi-Teja-S <ravithejatheja03@gmail.com>
2025-07-12 13:46:14 +0530

    Update urls.py

--- mysite/paykaro_app/urls.py
+++ mysite/paykaro_app/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('employee/salary/', views.view_salary, name='view_salary'),
     path('update-profile-photo/', views.update_profile_photo, name='update_profile_photo'),
     path('generate-payroll/', views.generate_payroll, name='generate_payroll'),
+    path('media/<path:path>', views.serve_media),
 
 ]
