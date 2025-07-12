e74e1c1a32b92f230e142cc0c813671ceeb82429
Ravi-Teja-S <ravithejatheja03@gmail.com>
2025-07-12 12:38:49 +0530

    Update build.sh

--- mysite/build.sh
+++ mysite/build.sh
@@ -3,6 +3,9 @@
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
+echo "Loading new migrations"
+python manage.py makemigrations
+
 echo "Running migrations..."
 python manage.py migrate --noinput
 
